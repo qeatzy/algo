c471e528015437d183998198f84510480c23c62b
U-QEATZY\Dell <qeatzy@users.noreply.github.com>
2017-01-17 17:12:53 +0800

    rotate. fix.

--- blockSwap.cpp
+++ blockSwap.cpp
@@ -1,4 +1,5 @@
 #include "utility.h"
+#include <list>
 
 // http://en.cppreference.com/w/cpp/algorithm/rotate
 // currently 'possible implementation' fails on corner case for std::rotate on cppreference.
@@ -10,15 +11,16 @@
 // if first == mid || mid == last, no operation is needed. eg, first + 1 == last.
 // search for __rotate in file /usr/lib/gcc/i686-pc-cygwin/5.4.0/include/c++/bits/stl_algo.h
 // a sensible return value is first + (last - mid). since c++11.
+
 template <typename Iterator>
-void swapBlock(Iterator first, Iterator mid, Iterator last) {
-    if (mid == last) return; // right half non-empty
+Iterator swapBlock_(Iterator first, Iterator mid, Iterator last, std::forward_iterator_tag) {
+    if (mid == last) return first; // right half non-empty
+    if (first == mid) return last;
     auto next = mid;
-    // precondition and loop invariant, right half are non-empty, mid != last.
-    // exit-condition of loop, exit after finish swap range if mid - fisrt == last - mid.
-    // progress, first always move forward.
+    auto ret = first;
+    while (next != last) { ++ret, ++next; }
+    next = mid;
     while (first != mid) { // left half non-empty
-        // std::iter_swap(first++, next++);
         std::iter_swap(first, next);
         ++first, ++next;
         if (next == last) { // right half exhausted, reset
@@ -26,10 +28,33 @@ void swapBlock(Iterator first, Iterator mid, Iterator last) {
         } else if (first == mid) { // left half exhausted, reset
             mid = next;
         }
-        // cout << "left = " << *first << ", next = " << *next << endl;
-        // wait();
-        // print(p, last);
     }
+    return ret;
+}
+
+template <typename Iterator>
+Iterator swapBlock_(Iterator first, Iterator mid, Iterator last, std::random_access_iterator_tag) {
+    if (mid == last) return first; // right half non-empty
+    auto sz_left = mid - first;
+    auto next = mid;
+    // precondition and loop invariant, right half are non-empty, mid != last.
+    // exit-condition of loop, exit after finish swap range if mid - fisrt == last - mid.
+    // progress, first always move forward.
+    while (first != mid) { // left half non-empty
+        std::iter_swap(first, next);
+        ++first, ++next;
+        if (next == last) { // right half exhausted, reset
+            next = mid;
+        } else if (first == mid) { // left half exhausted, reset
+            mid = next;
+        }
+    }
+    return last - sz_left;
+}
+
+template <typename Iterator>
+Iterator swapBlock(Iterator first, Iterator mid, Iterator last) {
+    return swapBlock_(first, mid, last, typename std::iterator_traits<Iterator>::iterator_category());
 }
 
 auto test_case() {
@@ -45,8 +70,14 @@ int main() {
     for (auto& v: test_case()) {
         print(v);
         // swapBlock(v.begin(), v.begin() + 2, v.end());
-        swapBlock(v.begin(), v.begin() + 7, v.end());
+        auto it = swapBlock(v.begin(), v.begin() + 7, v.end());
         // swapBlock(v.begin(), v.end(), v.end());
         print(v);
+        cout << "next start point is " << *it << endl;
     }
+    std::list<int> l = { 7, 5, 16, 8 };
+    print(l);
+    auto it = swapBlock(l.begin(), ++decltype(l.begin())(l.begin()), l.end());
+    print(l);
+    cout << "next start point is " << *it << endl;
 }
