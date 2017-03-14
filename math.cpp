4505fc61ed3c0d71a229cf2e1b9214ed8c020cfa
U-QEATZY\Dell <qeatzy@users.noreply.github.com>
2017-03-14 08:59:21 +0800

    add sqrt.cpp

--- math.cpp
+++ math.cpp
@@ -1,5 +1,19 @@
 #include "utility.h"
 
+// 453. Minimum Moves to Equal Array Elements https://leetcode.com/problems/minimum-moves-to-equal-array-elements/#/description
+// Given a non-empty integer array of size n, find the minimum number of moves required to make all array elements equal, where a move is incrementing n - 1 elements by 1.
+// Example: Input: [1,2,3] Output: 3
+// Explanation: Only three moves are needed (remember each move increments two elements): [1,2,3] => [2,3,3] => [3,4,3] => [4,4,4]
+// -- "cp(change perspective), to make all equal, increment all others has same effect as decrement the largest.
+    int minMoves(vector<int>& nums) {
+        int res = 0;
+        if (!nums.empty()) {
+            auto min = *std::min_element(nums.begin(), nums.end());
+            for (auto x: nums) res += x - min;
+        }
+        return res;
+    }
+
 // 231. Power of Two https://leetcode.com/problems/power-of-two/?tab=Description
     bool isPowerOfTwo(int n) {
         // return (n&(n-1)) == 0 && n > 0;
