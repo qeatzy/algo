d8700121b8c5501081ae6d7fb489b297716b2862
U-QEATZY\Dell <qeatzy@users.noreply.github.com>
2017-03-15 22:57:08 +0800

    add hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -32,7 +32,8 @@
 //     What if the given array is already sorted? How would you optimize your algorithm?
 //     What if nums1's size is small compared to nums2's size? Which algorithm is better?
 //     What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
-// TODO 
+// TODO  The intersection of two sorted arrays  http://stackoverflow.com/a/4601106/3625404
+// http://articles.leetcode.com/here-is-phone-screening-question-from/
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         vector<int> res;
         return res;
